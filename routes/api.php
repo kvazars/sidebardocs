46a128a2daf8bcbff281827b3c9b9b692b22c010
Victor Konobeyev <mr.kvazar@gmail.com>
2024-10-28 09:13:32 +0500

    fix api

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AboutController;
 use App\Http\Controllers\AvailableController;
 use App\Http\Controllers\ContentController;
 use App\Http\Controllers\GroupController;
@@ -19,25 +20,28 @@ Route::middleware(['auth:sanctum'])->group(function () {
     Route::get("/userFolder", [TreeController::class, "userFolder"]);
     Route::get('/logout', [UserController::class, 'logout']);
     //admin,ceo
-    Route::post("/resource", [ContentController::class, "saveResource"]);
-    Route::post("/saveImageByUrl", [ContentController::class, "saveImageByUrl"]);
-    Route::post("/saveImage", [ContentController::class, "saveImage"]);
-    Route::post("/saveFile", [ContentController::class, "saveFile"]);
-    Route::delete("/folder/{del}", [TreeController::class, "delete"]);
-    Route::delete("/resourcedel/{content}", [ContentController::class, "delResource"]);
-    Route::post("/folder", [TreeController::class, "store"]);
-    Route::post("/doc/{operation}/{id}", [TreeController::class, "upanddown"]);
+    Route::middleware(["role:admin|ceo"])->group(function () {
+        Route::post("/resource", [ContentController::class, "saveResource"]);
+        Route::post("/saveImageByUrl", [ContentController::class, "saveImageByUrl"]);
+        Route::post("/saveImage", [ContentController::class, "saveImage"]);
+        Route::post("/saveFile", [ContentController::class, "saveFile"]);
+        Route::delete("/folder/{del}", [TreeController::class, "delete"]);
+        Route::delete("/resourcedel/{content}", [ContentController::class, "delResource"]);
+        Route::post("/folder", [TreeController::class, "store"]);
+        Route::post("/doc/{operation}/{id}", [TreeController::class, "upanddown"]);
+    });
+
     //admin
-    Route::post("/user", [UserController::class, "store"]);
-    Route::put("/user/{id}", [UserController::class, "update"]);
-    Route::delete("/user/{id}", [UserController::class, "delete"]);
-    Route::get("/user", [UserController::class, "index"]);
-    Route::get("/group", [GroupController::class, "index"]);
-    Route::post("/group", [GroupController::class, "store"]);
-    Route::put("/group/{id}", [GroupController::class, "update"]);
-    Route::delete("/group/{id}", [GroupController::class, "delete"]);  
-    Route::post("/about", [AboutController::class, "store"]);
-    Route::get("/about", [AboutController::class, "index"]);
-
-
+    Route::middleware(["role:admin"])->group(function () {
+        Route::post("/user", [UserController::class, "store"]);
+        Route::put("/user/{id}", [UserController::class, "update"]);
+        Route::delete("/user/{id}", [UserController::class, "delete"]);
+        Route::get("/user", [UserController::class, "index"]);
+        Route::get("/group", [GroupController::class, "index"]);
+        Route::post("/group", [GroupController::class, "store"]);
+        Route::put("/group/{id}", [GroupController::class, "update"]);
+        Route::delete("/group/{id}", [GroupController::class, "delete"]);
+        Route::post("/about", [AboutController::class, "store"]);
+        Route::get("/about", [AboutController::class, "index"]);
+    });
 });
