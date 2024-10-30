ac56d7435e219916386a75cafd90d8d6699c2112
Victor Konobeyev <mr.kvazar@gmail.com>
2024-10-30 09:58:32 +0500

    fix

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,10 @@
-import './bootstrap';
+
+import 'bootstrap';
+import { createApp } from 'vue';
+
+const app = createApp({});
+
+import ExampleComponent from './components/ExampleComponent.vue';
+app.component('example-component', ExampleComponent);
+
+app.mount('#app');
--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,74 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+import vue from "@vitejs/plugin-vue";
+import autoprefixer from "autoprefixer";
+import path from "node:path";
 
 export default defineConfig({
+    base: "./",
+    css: {
+        postcss: {
+            plugins: [
+                autoprefixer({}), // add options if needed
+            ],
+        },
+        preprocessorOptions: {
+            scss: {
+                quietDeps: true,
+                api: "modern-compiler",
+            },
+        },
+    },
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ["resources/sass/app.scss", "resources/js/app.js"],
             refresh: true,
         }),
+        vue({
+            template: {
+                transformAssetUrls: {
+                    base: null,
+                    includeAbsolute: false,
+                },
+            },
+        }),
     ],
+    // resolve: {
+    //     alias: [
+    //         // webpack path resolve to vitejs
+    //         {
+    //             find: /^~(.*)$/,
+    //             replacement: "$1",
+    //         },
+    //         {
+    //             find: "@/",
+    //             replacement: `${path.resolve(__dirname, "src")}/`,
+    //         },
+    //         {
+    //             find: "@",
+    //             replacement: path.resolve(__dirname, "/src"),
+    //         },
+    //     ],
+    //     extensions: [
+    //         ".mjs",
+    //         ".js",
+    //         ".ts",
+    //         ".jsx",
+    //         ".tsx",
+    //         ".json",
+    //         ".vue",
+    //         ".scss",
+    //     ],
+    // },
+    server: {
+        port: 3001,
+        proxy: {
+            // https://vitejs.dev/config/server-options.html
+        },
+    },
+    resolve: {
+        alias: {
+            vue: 'vue/dist/vue.esm-bundler.js',
+        },
+    },
 });
